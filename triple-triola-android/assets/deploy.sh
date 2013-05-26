b1e56314a81d6bc0d5a6c78f53ff6d4a71a365b5
Łukasz Wieczorek <wieczorek1990@gmail.com>
2013-05-26 14:49:38 +0200

    more assets

--- triple-triola-android/assets/deploy.sh
+++ triple-triola-android/assets/deploy.sh
@@ -5,6 +5,11 @@ cd cards
 for file in *.svg; do
 	echo $file
 	filename=`basename "$file"`
-	newfile="${filename%.*}"'.jpg'
-	convert "$file" -resize "$RESIZE" "$newfile"
+	newfilename="${filename%.*}"
+	newfile="$newfilename"'.jpg'
+	blue="$newfilename"'-blue.jpg'
+	red="$newfilename"'-red.jpg'
+	convert -resize "$RESIZE" "$file" "$newfile"
+	convert -background 'rgb(104, 125, 207)' -resize "$RESIZE" "$file" "$blue"
+	convert -background 'rgb(249, 47, 16)' -resize "$RESIZE" "$file" "$red"
 done
